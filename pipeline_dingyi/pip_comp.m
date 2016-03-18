ffa00ac080880244bfc50204ec27bcd108dabc1b
lix90 <xiang.li.90@outlook.com>
2016-03-18 15:11:39 +0800

    modified pip_comp.m

--- pipeline_dingyi/pip_comp.m
+++ pipeline_dingyi/pip_comp.m
@@ -12,6 +12,8 @@ marksDir = fullfile(baseDir, 'marks');
 poolsize = 4;
 restLength = 30;
 cut = 0;
+tags = {'rest', 'task1', 'task2', 'task3'};
+marks = {'1', '4', '5', '6'};
 eeglabDir = fileparts(which('eeglab.m'));
 addpath(genpath(eeglabDir));
 % prepare datasets
@@ -33,78 +35,52 @@ for i = 1:1
     if restDuration < restLength; disp('resting time is too short'); continue; ...
     end
     pvafOut = struct();
-    rest = struct();
-    task1 = struct();
-    task2 = task1;
-    task3 = task1;
+    out = struct();
     name = strcat(ID{i}, '_spec.mat');
-    outNameRest = fullfile(outputDir, strcat('rest_', name));
-    outNameTask1 = fullfile(outputDir, strcat('task1_', name));
-    outNameTask2 = fullfile(outputDir, strcat('task2_', name));
-    outNameTask3 = fullfile(outputDir, strcat('task3_', name));
-    outNamePvaf = fullfile(outputDir, strcat('pvaf_', name));
-    % check if file exists
     fprintf('Loading (%i/%i %s)\n', i, nFile, fileName{i});
     % loading
     EEG = pop_loadset('filename', fileName{i}, 'filepath', inputDir);
-    EEG = eeg_checkset(EEG);
+    [ALLEEG EEG CURRENTSET = eeg_store(ALLEEG, EEG, 0);
+     EEG = eeg_checkset(EEG);
     % compute pvafs
-    if isempty(EEG.icaact)
+     if isempty(EEG.icaact)
         EEG.icaact = eeg_getdatact(EEG, ...
                                    'component', 1:size(EEG.icaweights, 1));
-    end
-    [pvaf, pvafs, vars] = eeg_pvaf(EEG, find(~EEG.reject.gcompreject), 'plot', 'off');
-    pvafOut.pvaf = pvaf;
-    pvafOut.pvafs = pvafs;
-    pvafOut.vars = vars;
-    parsave(outNamePvaf, pvafOut);
-    % remove artfactual ICs
-    EEG = pop_subcomp(EEG, find(EEG.reject.gcompreject), 0);
-    EEG = eeg_checkset(EEG);
-    % interpolate bad channels
-    power.chanlocs = EEG.etc.origchanlocs;
-    EEG = eeg_interp(EEG, EEG.etc.origchanlocs, 'spherical');
-    EEG = eeg_checkset(EEG);
-    % epoch 1: rest
-    % try
-    disp('spectral analysis of rest')
-    EEGrest = pop_epoch(EEG, {'1'}, [restDuration-restLength, restDuration]);
-    EEGrest = eeg_checkset(EEGrest);
-    [rest.spec, rest.freq, ~, ~, ~] = spectopo(EEGrest.data, 0, EEGrest.srate);
-    parsave(outNameRest, rest);
-    % catch
-    %     continue
-    % end
-    % epoch 2: task 1
-    % try
-    disp('spectral analysis of task 1')
-    EEGtask1 = pop_epoch(EEG, {'4'}, [0+cut, duration(2, i)-cut]); % 5
-    EEGtask1 = eeg_checkset(EEGtask1);
-    [task1.spec, task1.freq, ~, ~, ~] = spectopo(EEGtask1.data, 0, EEGtask1.srate);
-    parsave(outNameTask1, task1);
-    % catch
-    %     continue
-    % end
-    % try
-    % epoch 3: task 2
-    disp('spectral analysis of task 2')
-    EEGtask2 = pop_epoch(EEG, {'5'}, [0+cut, duration(3, i)-cut]); % 6
-    EEGtask2 = eeg_checkset(EEGtask2);
-    [task2.spec, task2.freq, ~, ~, ~] = spectopo(EEGtask2.data, 0, EEGtask2.srate);
-    parsave(outNameTask2, task2);
-    % catch
-    %     continue
-    % end
-    % try
-    % epoch 4: task 3
-    disp('spectral analysis of task 3')
-    EEGtask3 = pop_epoch(EEG, {'6'}, [0+cut, duration(4, i)-cut]); % 7
-    EEGtask3 = eeg_checkset(EEGtask3);
-    [task3.spec, task3.freq, ~, ~, ~] = spectopo(EEGtask3.data, 0, EEGtask3.srate);
-    parsave(outNameTask3, task3);
-    % catch
-    %     continue
-    % end
-    EEG = [];
-
+     end
+     [pvaf, pvafs, vars] = eeg_pvaf(EEG, find(~EEG.reject.gcompreject), 'plot', 'off');
+     pvafOut.pvaf = pvaf;
+     pvafOut.pvafs = pvafs;
+     pvafOut.vars = vars;
+     parsave(outNamePvaf, pvafOut);
+     % remove artfactual ICs
+     EEG = pop_subcomp(EEG, find(EEG.reject.gcompreject), 0);
+     EEG = eeg_checkset(EEG);
+     % interpolate bad channels
+     power.chanlocs = EEG.etc.origchanlocs;
+     EEG = eeg_interp(EEG, EEG.etc.origchanlocs, 'spherical');
+     EEG = eeg_checkset(EEG);
+     % epoch
+     tags = {'rest', 'task1', 'task2', 'task3'};
+     marks = {'1', '4', '5', '6'};
+     for itag = 1:numel(tags)
+         fprintf('spectral analysis of %s\n', tags{itag});
+         outName = fullfile(outputDir, strcat(tags{itag}, '_', name));
+         if itag == 1 % rest
+             EEG = pop_epoch(EEG, marks{itag}, ...
+                             [restDuration-restLength, restDuration]);
+             EEG = eeg_checkset(EEG);
+         else
+             EEG = pop_epoch(EEG, marks{itag}, ...
+                             [0+cut, duration(itag, i)-cut]);
+         end
+         [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, itag, 'gui', 'off');
+         EEG = eeg_checkset(EEG);
+         EEG = pop_rmbase(EEG, []);
+         [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, itag, 'gui', 'off');
+         [out.spec, out.freq, ~, ~, ~] = spectopo(EEG.data, 0, EEG.srate, ...
+                                                  'plot', 'off');
+         parsave(outName, out);
+         [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, itag, 'retrieve', 1);
+     end
+     EEG = []; ALLEEG = []; CURRENTSET = 0;
 end
