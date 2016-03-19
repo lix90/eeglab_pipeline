79610b97c8025a00e7df1c4b22df6c800d13cf48
lix90 <xiang.li.90@outlook.com>
2016-03-20 01:02:54 +0800

    plotting scripts

--- pipeline_dingyi/errorbarPlot.m
+++ pipeline_dingyi/errorbarPlot.m
@@ -1,30 +1,106 @@
 inDir = '~/Data/dingyi/output_noRemoveWindows_1hz/';
 outDir = '~/Data/dingyi/image_noRemoveWindows_1hz/';
 
-load(fullfile(inDir, 'dingyi_specdata.mat'));
+load(fullfile(inDir, 'Pow.mat'));
+load(fullfile(inDir, 'samEn.mat'));
+load(fullfile(inDir, 'chanlocs.mat'));
 
-chan = 'FZ';
-band = 'beta';
+%% relative power
 
-TITLE = sprintf('%s %s', chan, band);
-LINEWIDTH = 4;
-YLABEL = 'Relative Power (dB)';
-COLOR = 'r';
-
-tagBands = out.tagBands;
-tagStates = out.tagStates;
-chanlocs = out.chanlocs;
-
-indBand = ismember(tagBands, band);
+chan = {'CZ'};
+tagBands = Pow.tagBands(1:5);
+tagStates = Pow.tagStates;
 indChan = ismember({chanlocs.labels}, chan);
 
-dataSubset = squeeze(out.specdata(indBand, :, indChan, :));
-dataMN = mean(dataSubset, 2);
-dataSE = std(dataSubset,1,2)/sqrt(size(dataSubset,2));
+relPlot = squeeze(mean(Pow.Rel(:, 1:5, indChan, :), 3));
+dataMN = squeeze(mean(relPlot, 3));
+dataSE = std(relPlot,1,3)/sqrt(size(relPlot,3));
 
 h = errorbar(dataMN, dataSE);
-set(h, 'linewidth', LINEWIDTH, 'color', COLOR);
+% [hbar, herr] = barwitherr(dataSE', dataMN');
+set(h, 'linewidth', 2);
 set(gca, 'xticklabel', tagStates, 'xtick', [1:4]);
 set(gcf, 'color', 'w');
-title(TITLE);
-ylabel(YLABEL);
+legend(h, 'string', tagBands, 'location', 'SouthEastOutside');
+title('Relative Power');
+ylabel('Relative Power');
+
+%% sample entropy
+
+chan = {'CZ'};
+tagBands = Pow.tagBands(1:5);
+tagStates = Pow.tagStates;
+indChan = ismember({chanlocs.labels}, chan);
+
+SEPlot = squeeze(mean(sampleEn.data(:, indChan, :), 2));
+dataMN = squeeze(mean(SEPlot, 2));
+dataSE = std(SEPlot,1,2)/sqrt(size(SEPlot,2));
+
+h = errorbar(dataMN, dataSE);
+% [hbar, herr] = barwitherr(dataSE', dataMN');
+set(h, 'linewidth', 2);
+set(gca, 'xticklabel', tagStates, 'xtick', [1:4]);
+set(gcf, 'color', 'w');
+% legend(h, 'string', tagBands, 'location', 'SouthEastOutside');
+% title('Relative Power');
+% ylabel('Relative Power');
+
+%% Delta/alpha
+
+chan = {'CZ'};
+tagBands = Pow.tagBands(1:5);
+tagStates = Pow.tagStates;
+indChan = ismember({chanlocs.labels}, chan);
+
+DAPlot = squeeze(mean(Pow.DA(:, indChan, :), 2));
+dataMN = squeeze(mean(DAPlot, 2));
+dataSE = std(DAPlot,1,2)/sqrt(size(DAPlot,2));
+
+h = errorbar(dataMN, dataSE);
+% [hbar, herr] = barwitherr(dataSE', dataMN');
+set(h, 'linewidth', 2);
+set(gca, 'xticklabel', tagStates, 'xtick', [1:4]);
+set(gcf, 'color', 'w');
+% legend(h, 'string', tagBands, 'location', 'SouthEastOutside');
+% title('Relative Power');
+% ylabel('Relative Power');
+
+%% Delta+theta / alpha+theta
+
+chan = {'CZ'};
+tagBands = Pow.tagBands(1:5);
+tagStates = Pow.tagStates;
+indChan = ismember({chanlocs.labels}, chan);
+
+DTATPlot = squeeze(mean(Pow.DTAT(:, indChan, :), 2));
+dataMN = squeeze(mean(DTATPlot, 2));
+dataSE = std(DTATPlot,1,2)/sqrt(size(DTATPlot,2));
+
+h = errorbar(dataMN, dataSE);
+% [hbar, herr] = barwitherr(dataSE', dataMN');
+set(h, 'linewidth', 2);
+set(gca, 'xticklabel', tagStates, 'xtick', [1:4]);
+set(gcf, 'color', 'w');
+% legend(h, 'string', tagBands, 'location', 'SouthEastOutside');
+% title('Relative Power');
+% ylabel('Relative Power');
+
+%% Delta+theta / alpha+theta
+
+chan = {'CZ'};
+tagBands = Pow.tagBands(1:5);
+tagStates = Pow.tagStates;
+indChan = ismember({chanlocs.labels}, chan);
+
+DTABPlot = squeeze(mean(Pow.DTAB(:, indChan, :), 2));
+dataMN = squeeze(mean(DTABPlot, 2));
+dataSE = std(DTABPlot,1,2)/sqrt(size(DTABPlot,2));
+
+h = errorbar(dataMN, dataSE);
+% [hbar, herr] = barwitherr(dataSE', dataMN');
+set(h, 'linewidth', 2);
+set(gca, 'xticklabel', tagStates, 'xtick', [1:4]);
+set(gcf, 'color', 'w');
+% legend(h, 'string', tagBands, 'location', 'SouthEastOutside');
+% title('Relative Power');
+% ylabel('Relative Power');
--- pipeline_dingyi/topoPlot.m
+++ pipeline_dingyi/topoPlot.m
@@ -1,32 +1,56 @@
 inDir = '~/Data/dingyi/output_noRemoveWindows_1hz/';
 outDir = '~/Data/dingyi/image_noRemoveWindows_1hz/';
 
-load(fullfile(inDir, 'dingyi_specdata.mat'));
+load(fullfile(inDir, 'Pow.mat'));
+load(fullfile(inDir, 'chanlocs.mat'));
+load(fullfile(inDir, 'samEn.mat'));
 
-TITLE = sprintf('%s', band);
-LINEWIDTH = 4;
-YLABEL = 'Relative Power (dB)';
-COLOR = 'r';
+tagBands = Pow.tagBands(1:5);
+tagStates = Pow.tagStates;
 
-tagBands = out.tagBands;
-tagStates = out.tagStates;
-chanlocs = out.chanlocs;
-
-for i = 1:numel(tagBands)
-    
-    dataSubset = squeeze(mean(out.specdata(i, :, :, :), 4));
-    dataplot = {squeeze(dataSubset(1,:)), ...
-                squeeze(dataSubset(2,:)), ...
-                squeeze(dataSubset(3,:)), ...
-                squeeze(dataSubset(4,:))};
+%% Relative Power
+for iBand = 1:numel(tagBands)
+    tmp = squeeze(mean(Pow.Rel(:, iBand, :, :), 4));
+    dataplot = {squeeze(tmp(1,:)), squeeze(tmp(2,:));
+                squeeze(tmp(3,:)), squeeze(tmp(4,:))};
     std_chantopo(dataplot,'chanlocs', chanlocs,...
                  'datatype', 'spec',...
-                 'titles', tagStates);
-    set(gcf, 'name', tagBands(i));
-    % h = errorbar(dataMN, dataSE);
-    % set(h, 'linewidth', LINEWIDTH, 'color', COLOR);
-    % set(gca, 'xticklabel', tagStates, 'xtick', [1:4]);
-    % set(gcf, 'color', 'w');
-    % title(TITLE);
-    % ylabel(YLABEL);
+                 'titles', reshape(tagStates, [2,2]));
+    set(gcf, 'name', tagBands{iBand});
 end
+
+%% DA
+DA = squeeze(mean(Pow.DA,3));
+DAplot = {squeeze(DA(1,:)), squeeze(DA(2,:));
+          squeeze(DA(3,:)), squeeze(DA(4,:))};
+std_chantopo(DAplot, 'chanlocs', chanlocs, ...
+             'datatype', 'spec', ...
+             'title', reshape(tagStates, [2,2]));
+set(gcf, 'name', 'Delta/Alpha');
+
+%% DTAT
+DTAT = squeeze(mean(Pow.DTAT,3));
+DTATplot = {squeeze(DTAT(1,:)), squeeze(DTAT(2,:));
+            squeeze(DTAT(3,:)), squeeze(DTAT(4,:))};
+std_chantopo(DTATplot, 'chanlocs', chanlocs, ...
+             'datatype', 'spec', ...
+             'title', reshape(tagStates, [2,2]));
+set(gcf, 'name', '(Delta+Theta)/(Alpha+Theta)');
+
+%% DTAB
+DTAB = squeeze(mean(Pow.DTAB,3));
+DTABplot = {squeeze(DTAB(1,:)), squeeze(DTAB(2,:));
+            squeeze(DTAB(3,:)), squeeze(DTAB(4,:))};
+std_chantopo(DTABplot, 'chanlocs', chanlocs, ...
+             'datatype', 'spec', ...
+             'title', reshape(tagStates, [2,2]));
+set(gcf, 'name', '(Delta+Theta)/(Alpha+Beta)');
+
+%% sample entropy
+SE = squeeze(mean(sampleEn.data,3));
+SEplot = {squeeze(SE(1,:)), squeeze(SE(2,:));
+          squeeze(SE(3,:)), squeeze(SE(4,:))};
+std_chantopo(SEplot, 'chanlocs', chanlocs, ...
+             'datatype', 'spec', ...
+             'title', reshape(tagStates, [2,2]));
+set(gcf, 'name', 'sample entropy');
