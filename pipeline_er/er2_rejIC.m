0d2a6afcec9bddb2fee1314d3e2a5e8fdfcb0538
lix90 <its.lix@outlook.com>
2016-07-28 09:54:43 +0800

    change

--- pipeline_er/er2_rejIC.m
+++ pipeline_er/er2_rejIC.m
@@ -4,6 +4,7 @@ clear, clc, close all
 baseDir = '~/Data/gender-role-emotion-regulation/';
 % eeglabPath = '';
 inputTag = 'ica2';
+outputTag = 'rejIC';
 fileExtension = 'set';
 prefixPosition = 1;
 isTrial = 0;
@@ -11,20 +12,34 @@ EOG = [];
 
 %%---------
 inputDir = fullfile(baseDir, inputTag);
-% outputDir = fullfile(baseDir, outputTag);
-% if ~exist(outputDir, 'dir'); mkdir(outputDir); end
+outputDir = fullfile(baseDir, outputTag);
+if ~exist(outputDir, 'dir'); mkdir(outputDir); end
 [inputFilename, id] = getFileInfo(inputDir, fileExtension, prefixPosition);
+
 % setEEGLAB;
 for i = 1:numel(id)
+    
+    outputFilename = sprintf('%s_%s.set', id{i}, outputTag);
+    outputFilenameFull = fullfile(outputDir, outputFilename);
+    
+    if exist(outputFilenameFull, 'file')
+        warning('files alrealy exist!')
+        continue
+    end
+
     % import dataset
     [EEG, ALLEEG, CURRENTSET] = importEEG(inputDir, inputFilename{i});
+
     % identify bad ICs
     try
         EEG = rejBySASICA(EEG, EOG);
     catch
         disp('wrong');
     end
+    
     % save dataset
-    EEG = pop_saveset(EEG, 'savemode', 'resave');
+    EEG = pop_saveset(EEG, 'filename', outputFilenameFull);
+    % EEG = pop_saveset(EEG, 'savemode', 'resave');
     EEG = []; ALLEEG = []; CURRENTSET = [];
+    
 end
