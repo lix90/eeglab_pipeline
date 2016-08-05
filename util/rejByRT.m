2530f0f01a664c827b14f2c8a8fdbedabdfec7af
lix90 <its.lix@outlook.com>
2016-08-05 17:13:31 +0800

    changes

---
+++ util/rejByRT.m
@@ -0,0 +1,20 @@
+function rej = rejByRT(EEG, resp, timewin)
+
+if ~iscellstr(resp)
+    disp('resp must be cellstr');
+    return;
+end
+
+if length(timewin)~=2
+    disp('timewin must have two elements');
+    return;
+end
+
+ntrials = EEG.trials;
+rej = zeros(1, ntrials);
+for i = 1:numel(resp)
+    rt = eeg_getepochevent(EEG, 'type', resp{i}, 'timewin', timewin);
+    rt = isnan(rt);
+    rej = rt+rej;
+end
+rej = logical(rej);
